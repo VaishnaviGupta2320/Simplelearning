cd162439c64b6bb9d85253dceee734fad7906c8a
Vaishnavi Gupta <150041663+VaishnaviGupta2320@users.noreply.github.com>
2024-12-19 16:46:08 +0530

    Learned funtionality on text compose and modifier. Also how to provide various funtionality like padding,style, height, width, clickable to the text.

--- app/src/main/java/com/example/simplelearning/MainActivity.kt
+++ app/src/main/java/com/example/simplelearning/MainActivity.kt
@@ -4,14 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.simplelearning.ui.theme.SimplelearningTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +33,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
 
-                 //   GreetingText(name = "Android")
-            GreetingButton()
+                   GreetingText(name = "Android")
+           // GreetingButton()
 
                 }
             }
@@ -30,19 +43,30 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText(name: String, modifier: Modifier = Modifier) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!", modifier = Modifier.height(240.dp).
+    width(240.dp)
+        .clickable(onClick =  {  })
+        .padding(all = 50.dp),
+//        style = TextStyle(color = Color.Green,
+//            fontWeight = FontWeight.Bold, fontStyle = FontStyle.Italic, fontSize = 20.sp)
+        style = MaterialTheme.typography.headlineMedium, fontWeight = FontWeight.ExtraBold
+    )
+   // Text(text = "Hello $name!", modifier = Modifier.size(width = 80.dp, height = 240.dp))
+    //Text(text = "Hello $name!", modifier = Modifier.fillMaxSize())
+   // Text(text = "Hello $name!", modifier = Modifier.fillMaxWidth(1f))
+   //Text(text = "Hello $name!", modifier = Modifier.fillMaxHeight(0.5f))
 }
-@Composable
-fun GreetingButton()
-{
-    Button(onClick = { }) {
-GreetingText(name = "Button")
-    }
-}
+//@Composable
+//fun GreetingButton()
+//{
+//    Button(onClick = { }) {
+//GreetingText(name = "Button")
+//    }
+//}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-        //GreetingText("Android")
-    GreetingButton()
+        GreetingText("Android")
+   // GreetingButton()
     }
